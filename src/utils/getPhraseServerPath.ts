a88826d296586465f7e2cbb33b080c4a9b1a84bf
DavidOriginalnoeImya <abulashvili@inbox.ru>
2023-11-21 19:04:08 +0400

    feat: изменить название расширения и путь к серверу

--- src/utils/getPhraseServerPath.ts
+++ src/utils/getPhraseServerPath.ts
@@ -1,6 +1,6 @@
 
 const getPhraseServerPath = (postfix: string) => {
-    return "http://localhost:8086" + postfix;
+    return "http://localhost:8090" + postfix;
 }
 
 export default getPhraseServerPath;
